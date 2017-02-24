e237462219ddde916415e6cabf4af8d9695b550d
Nicolás López <nicolaslopezj@me.com>
2017-02-24 11:32:51 -0300

    publish support for graphqls

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orionsoft:graphql-compiler',
   summary: 'Write graphql in .graphql files',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/orionsoft/graphql-compiler'
 })
 
